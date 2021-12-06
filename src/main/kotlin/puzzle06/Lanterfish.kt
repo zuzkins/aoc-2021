0a52d9364950f14d03f4ba07688e399097e7c952
Jiri Zuna <jiri@zunovi.cz>
2021-12-06 14:39:54 +0100

    Puzzle 06 - part 1 optimized implementation

--- src/main/kotlin/puzzle06/Lanterfish.kt
+++ src/main/kotlin/puzzle06/Lanterfish.kt
@@ -2,24 +2,32 @@ package puzzle06
 
 object Lanterfish {
 
-    fun day(population: List<Int>): List<Int> {
-        var spawned = 0
-        return population.map { age ->
-            if (age == 0) {
-                spawned++
-                6
-            } else {
-                age - 1
-            }
-        } + generateSequence { 8 }.take(spawned)
+    fun day(population: Map<Int, Long>): Map<Int, Long> {
+        val newPopulation = prepareEmptyPopulation()
+
+        val breeders = population.getOrDefault(0, 0)
+        (1..8).forEach { stage ->
+            newPopulation[stage - 1] = population.getOrDefault(stage, 0)
+        }
+        newPopulation[8] = breeders
+        newPopulation[6] = newPopulation.getValue(6) + breeders
+        return newPopulation
     }
 
+    private fun prepareEmptyPopulation() = (0..8).associateWith { 0L }.toMutableMap()
 
-    fun List<Int>.nextDay(): List<Int> = day(this)
+    fun List<Int>.asPopulation(): Map<Int, Long> = this.fold(prepareEmptyPopulation()) { pop, stage ->
+        pop[stage] = pop.getOrDefault(stage, 0L) + 1L
+        pop
+    }
 
-    fun Int.daysOf(population: List<Int>): List<Int> {
+    fun Map<Int, Long>.nextDay(): Map<Int, Long> = day(this)
+
+    fun Int.daysOf(population: Map<Int, Long>): Map<Int, Long> {
         return (1..this).fold(population) { p, day ->
             p.nextDay()
         }
     }
+
+    fun Map<Int, Long>.countFish(): Long = values.sum()
 }
--- src/test/kotlin/puzzle06/LanterfishTest.kt
+++ src/test/kotlin/puzzle06/LanterfishTest.kt
@@ -3,40 +3,67 @@ package puzzle06
 import Utils
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import puzzle06.Lanterfish.asPopulation
+import puzzle06.Lanterfish.countFish
 import puzzle06.Lanterfish.daysOf
 import puzzle06.Lanterfish.nextDay
 
 internal class LanterfishTest {
 
     @Test
-    fun `first generation`() {
-        val population = listOf(3, 4, 3, 1, 2).nextDay()
+    fun `can create initial population`() {
+        val population = listOf(3, 4, 3, 1, 2).asPopulation()
 
-        assertThat(population).isEqualTo(listOf(2, 3, 2, 0, 1))
+        assertThat(population).isEqualTo(
+            mapOf(
+                0 to 0L,
+                1 to 1L,
+                2 to 1L,
+                3 to 2L,
+                4 to 1L,
+                5 to 0L,
+                6 to 0L,
+                7 to 0L,
+                8 to 0L,
+            )
+        )
+    }
+
+    @Test
+    fun `first generation`() {
+        val population = listOf(3, 4, 3, 1, 2).asPopulation().nextDay()
+
+        assertThat(population).isEqualTo(listOf(2, 3, 2, 0, 1).asPopulation())
     }
 
     @Test
     fun `second generation`() {
-        val population = listOf(3, 4, 3, 1, 2).nextDay().nextDay()
+        val population = listOf(3, 4, 3, 1, 2).asPopulation().nextDay().nextDay()
 
-        assertThat(population).isEqualTo(listOf(1, 2, 1, 6, 0, 8))
+        assertThat(population).isEqualTo(listOf(1, 2, 1, 6, 0, 8).asPopulation())
     }
 
-    val exampleInput = "3,4,3,1,2".split(",").map { it.toInt() }
+    @Test
+    fun `can use shortcut population`() {
+        val population = 2.daysOf(listOf(3, 4, 3, 1, 2).asPopulation())
+        assertThat(population).isEqualTo(listOf(1, 2, 1, 6, 0, 8).asPopulation())
+    }
+
+    val exampleInput = "3,4,3,1,2".split(",").map { it.toInt() }.asPopulation()
 
     @Test
     fun `example solution`() {
         val population18 = 18.daysOf(exampleInput)
-        assertThat(population18.size).isEqualTo(26)
+        assertThat(population18.countFish()).isEqualTo(26)
 
         val population80 = 80.daysOf(exampleInput)
-        assertThat(population80.size).isEqualTo(5934)
+        assertThat(population80.countFish()).isEqualTo(5934)
     }
 
     @Test
     fun `puzzle solution`() {
-        val input = Utils.readResource("/puzzle06/input").split(",").map { it.toInt() }
+        val input = Utils.readResource("/puzzle06/input").split(",").map { it.toInt() }.asPopulation()
         val population = 80.daysOf(input)
-        assertThat(population.size).isEqualTo(354564)
+        assertThat(population.countFish()).isEqualTo(354564)
     }
 }
