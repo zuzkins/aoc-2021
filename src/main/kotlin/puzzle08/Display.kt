43d6bcf828c2693e28c8fa3d939956a9a287755c
Jiri Zuna <jiri@zunovi.cz>
2021-12-08 20:00:14 +0100

    Puzzle 08 - part 2

--- src/main/kotlin/puzzle08/Display.kt
+++ src/main/kotlin/puzzle08/Display.kt
@@ -1,16 +1,16 @@
 package puzzle08
 
 val originalWiring = mapOf(
-    0 to listOf('a', 'b', 'c', 'e', 'f', 'g'),
-    1 to listOf('c', 'f'),
-    2 to listOf('a', 'c', 'd', 'e', 'g'),
-    3 to listOf('a', 'c', 'd', 'f', 'g'),
-    4 to listOf('b', 'c', 'd', 'f'),
-    5 to listOf('a', 'b', 'd', 'f', 'g'),
-    6 to listOf('a', 'b', 'd', 'e', 'f', 'g'),
-    7 to listOf('a', 'c', 'f'),
-    8 to listOf('a', 'b', 'c', 'd', 'e', 'f', 'g'),
-    9 to listOf('a', 'b', 'c', 'd', 'f', 'g'),
+    0 to listOf('a', 'b', 'c', 'e', 'f', 'g'),      // 6
+    1 to listOf('c', 'f'),                          // 2
+    2 to listOf('a', 'c', 'd', 'e', 'g'),           // 5
+    3 to listOf('a', 'c', 'd', 'f', 'g'),           // 5
+    4 to listOf('b', 'c', 'd', 'f'),                // 4
+    5 to listOf('a', 'b', 'd', 'f', 'g'),           // 5
+    6 to listOf('a', 'b', 'd', 'e', 'f', 'g'),      // 6
+    7 to listOf('a', 'c', 'f'),                     // 3
+    8 to listOf('a', 'b', 'c', 'd', 'e', 'f', 'g'), // 7
+    9 to listOf('a', 'b', 'c', 'd', 'f', 'g'),      // 6
 )
 
 val uniqueDigits = setOf(1, 4, 7, 8)
@@ -24,6 +24,74 @@ class Display {
             return uniqueDigits.singleOrNull { digit ->
                 requiredLength == originalWiring.getValue(digit).size
             } ?: error("Could not guess digit from given signals ($signals) by wire count $requiredLength")
+        }
+
+        fun discoverDigits(exampleSignals: List<String>): Map<Int, Set<Char>> {
+            val signals = exampleSignals.map { it.toCharArray().toSet() }
+            val one = signals.single { it.size == 2 }
+            val four = signals.single { it.size == 4 }
+            val seven = signals.single { it.size == 3 }
+            val eight = signals.single { it.size == 7 }
+
+            // NINE contains FOUR as the only 6-segment digit
+            val nine = signals.single { it.size == 6 && it.superseeds(four) }
+
+            // THREE contains SEVEN as the only 5-segment digit
+            val three = signals.single { it.size == 5 && it.superseeds(seven) }
+
+            // ZERO contains ONE as the only 6-segment digit
+            val zero = signals.single {
+                it.size == 6 && it.superseeds(one) && it.isNot(nine)
+            }
+
+            // SIX is the last 6-segment digit
+            val six = signals.single {
+                it.size == 6 && it.isNot(nine) && it.isNot(zero)
+            }
+
+            // FIVE is contained in a SIX as the only 5-segment digit
+            val five = signals.single {
+                it.size == 5 && six.superseeds(it)
+            }
+
+            // TWI is the last 5-segment digit
+            val two = signals.single {
+                it.size == 5 && it.isNot(five) && it.isNot(three)
+            }
+
+            return mapOf(
+                0 to zero,
+                1 to one,
+                2 to two,
+                3 to three,
+                4 to four,
+                6 to six,
+                5 to five,
+                7 to seven,
+                8 to eight,
+                9 to nine,
+            )
+        }
+
+        fun String.toSet() = toCharArray().toSet()
+        private fun Set<Char>.superseeds(other: Set<Char>) = containsAll(other)
+        private fun Set<Char>.isNot(other: Set<Char>) = !containsAll(other)
+
+        fun Map<Int, Set<Char>>.decode(digit: String): Int {
+            val chars = digit.toSet()
+            return this.asIterable().single { it.value == chars }.key
+        }
+
+        fun Map<Int, Set<Char>>.readValue(digits: List<String>) = digits
+            .map { this.decode(it) }
+            .fold(0) { acc, cur ->
+                acc * 10 + cur
+            }
+
+        fun String.discoverAndRead(): Int {
+            val (signals, digits) = split(" | ")
+            val mapping = discoverDigits(signals.split(" "))
+            return mapping.readValue(digits.split(" "))
         }
     }
 }
--- src/test/kotlin/puzzle08/DisplayTest.kt
+++ src/test/kotlin/puzzle08/DisplayTest.kt
@@ -3,6 +3,10 @@ package puzzle08
 import Utils
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import puzzle08.Display.Companion.decode
+import puzzle08.Display.Companion.discoverAndRead
+import puzzle08.Display.Companion.readValue
+import puzzle08.Display.Companion.toSet
 
 internal class DisplayTest {
 
@@ -47,5 +51,54 @@ internal class DisplayTest {
     fun `puzzle 1 solution`() {
         val signals = Utils.readResource("/puzzle08/input").lines().map { it.split(" | ")[1] }.flatMap { it.split(" ") }
         assertThat(guessNumbers(signals).size).isEqualTo(255)
+    }
+
+    val exampleSignals = "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab".split(" ")
+
+    @Test
+    fun `can discover basic numbers`() {
+        val digits = Display.discoverDigits(exampleSignals)
+
+        assertThat(digits[1]).isEqualTo("ab".toSet())
+        assertThat(digits[4]).isEqualTo("eafb".toSet())
+        assertThat(digits[7]).isEqualTo("dab".toSet())
+        assertThat(digits[8]).isEqualTo("acedgfb".toSet())
+        assertThat(digits[9]).isEqualTo("cefabd".toSet())
+        assertThat(digits[3]).isEqualTo("fbcad".toSet())
+        assertThat(digits[0]).isEqualTo("cagedb".toSet())
+        assertThat(digits[6]).isEqualTo("cdfgeb".toSet())
+        assertThat(digits[5]).isEqualTo("cdfbe".toSet())
+        assertThat(digits[2]).isEqualTo("gcdfa".toSet())
+    }
+
+    @Test
+    fun `can decode discovered digits`() {
+        val digits = Display.discoverDigits(exampleSignals)
+
+        assertThat(digits.decode("cdfeb")).isEqualTo(5)
+        assertThat(digits.decode("fcadb")).isEqualTo(3)
+    }
+
+    @Test
+    fun `can calculate value from display`() {
+        val digits = Display.discoverDigits(exampleSignals)
+        assertThat(digits.readValue(listOf("cdfeb", "fcadb", "cdfeb", "cdbaf"))).isEqualTo(5353)
+    }
+
+    @Test
+    fun `can discover and read digits from display`() {
+        assertThat("acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf".discoverAndRead()).isEqualTo(
+            5353
+        )
+    }
+
+    @Test
+    fun `example solution 2`() {
+        assertThat(exampleInput.lines().sumOf { it.discoverAndRead() }).isEqualTo(61229)
+    }
+
+    @Test
+    fun `puzzle solution 2`() {
+        assertThat(Utils.readResource("/puzzle08/input").lines().sumOf { it.discoverAndRead() }).isEqualTo(982158)
     }
 }
