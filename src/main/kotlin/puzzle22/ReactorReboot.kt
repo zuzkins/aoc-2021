8eb352d5ec1a670dd7855210829c8aba7fc54e24
Jiri Zuna <jiri@zunovi.cz>
2022-01-04 21:12:46 +0100

    Puzzle 22 - part 2
    Solve it by abusing the fact, that we can insert new cubes, which will make
    the total volume of cubes accurate, but do not require me to split cubes.
    Those are inserted at places where normally holes would be to accommodate
    the change in resulting volume.

--- src/main/kotlin/puzzle22/ReactorReboot.kt
+++ src/main/kotlin/puzzle22/ReactorReboot.kt
@@ -1,5 +1,8 @@
 package puzzle22
 
+import arrow.core.None
+import arrow.core.Option
+import arrow.core.Some
 import java.util.*
 
 fun IntRange.intersection(other: IntRange): IntRange {
@@ -32,7 +35,31 @@ fun Triple<IntRange, IntRange, IntRange>.forAll(f: (x: Int, y: Int, z: Int) -> U
     }
 }
 
-data class Cuboid(val x: IntRange, val y: IntRange, val z: IntRange, val toggle: Boolean)
+data class Cuboid(val x: IntRange, val y: IntRange, val z: IntRange, val toggle: Boolean) {
+    fun intersect(other: Cuboid): Option<Cuboid> {
+        val iX = x.intersection(other.x)
+        val iY = y.intersection(other.y)
+        val iZ = z.intersection(other.z)
+        if (iX.isEmpty() || iY.isEmpty() || iZ.isEmpty()) return None
+
+        val finalToggle = when (this.toggle to other.toggle) {
+            true to true -> false
+            true to false -> false
+            false to true -> true
+            false to false -> true
+            else -> error("intersection of cuboids not supported")
+        }
+        return Some(Cuboid(iX, iY, iZ, finalToggle))
+    }
+
+    val volume by lazy {
+        val xSize = x.last.toLong() - x.first.toLong() + 1L
+        val ySize = y.last.toLong() - y.first.toLong() + 1L
+        val zSize = z.last.toLong() - z.first.toLong() + 1L
+        xSize * ySize * zSize * if (toggle) 1 else -1
+    }
+}
+
 data class Cube(
     val originX: Int,
     val originY: Int,
@@ -102,4 +129,33 @@ object ReactorReboot {
         val (from, to) = e.drop(2).split("..").map { it.toInt() }
         return IntRange(from, to)
     }
+
+    fun List<Cuboid>.process(): List<Cuboid> {
+        val result = LinkedList<Cuboid>()
+        this.forEach { curCuboid ->
+            val intersections = LinkedList<Cuboid>()
+            result.forEach { otherCuboid ->
+                when (val i = otherCuboid.intersect(curCuboid)) {
+                    None -> {
+                        // noop - no intersection with previous cuboid
+                    }
+                    is Some -> intersections.add(i.value)
+                }
+            }
+            result.addAll(intersections)
+            if (curCuboid.toggle) {
+                result.add(curCuboid)
+            }
+        }
+        return result
+    }
+
+    val List<Cuboid>.volume: Long
+        get() {
+            var volume = 0L
+            forEach {
+                volume += it.volume
+            }
+            return volume
+        }
 }
--- src/test/kotlin/puzzle22/ReactorRebootTest.kt
+++ src/test/kotlin/puzzle22/ReactorRebootTest.kt
@@ -1,9 +1,13 @@
 package puzzle22
 
+import arrow.core.None
+import arrow.core.Some
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import puzzle22.ReactorReboot.parseCuboid
+import puzzle22.ReactorReboot.process
+import puzzle22.ReactorReboot.volume
 import java.time.Instant
 import kotlin.math.abs
 import kotlin.math.ceil
@@ -134,6 +138,7 @@ internal class ReactorRebootTest {
     }
 
     @Test
+    @Disabled
     fun `can solve example by shifting cube`() {
         val cuboids = listOf(parseCuboid("on x=10..12,y=10..12,z=10..12"))
         val counter = processCuboids(cuboids, -50, 50, 3)
@@ -145,6 +150,112 @@ internal class ReactorRebootTest {
     @Disabled
     fun `can solve puzzle by shifting cube`() {
         assertThat(processCuboids(puzzleInput, -100000, 100000, 300)).isEqualTo(27)
+    }
+
+    @Test
+    fun `can compute differential intersection of 2 cuboids - on and off - 1`() {
+        val cuboidOn = parseCuboid("on x=1..5,y=1..5,z=1..5")
+        val cuboidOff = parseCuboid("off x=4..5,y=4..5,z=4..5")
+        assertThat(cuboidOn.intersect(cuboidOff)).isEqualTo(Some(parseCuboid("off x=4..5,y=4..5,z=4..5")))
+    }
+
+    @Test
+    fun `can compute differential intersection of 2 cuboids - on and off - 2`() {
+        val cuboidOn = parseCuboid("on x=1..5,y=1..5,z=1..5")
+        val cuboidOff = parseCuboid("off x=4..15,y=4..15,z=4..15")
+        assertThat(cuboidOn.intersect(cuboidOff)).isEqualTo(Some(parseCuboid("off x=4..5,y=4..5,z=4..5")))
+    }
+
+    @Test
+    fun `can compute differential intersection of 2 cuboids - on and off - 3`() {
+        val cuboidOn = parseCuboid("on x=1..15,y=1..15,z=1..15")
+        val cuboidOff = parseCuboid("off x=4..5,y=4..5,z=4..5")
+        assertThat(cuboidOn.intersect(cuboidOff)).isEqualTo(Some(parseCuboid("off x=4..5,y=4..5,z=4..5")))
+    }
+
+    @Test
+    fun `can compute differential intersection of 2 cuboids - on and off - 4`() {
+        val cuboidOn = parseCuboid("on x=1..5,y=1..5,z=1..5")
+        val cuboidOff = parseCuboid("off x=14..15,y=14..15,z=14..15")
+        assertThat(cuboidOn.intersect(cuboidOff)).isEqualTo(None)
+    }
+
+    @Test
+    fun `can compute differential intersection of 2 cuboids - on and on - 1`() {
+        val cuboidOn = parseCuboid("on x=1..15,y=1..15,z=1..15")
+        val cuboidOff = parseCuboid("on x=4..5,y=4..5,z=4..5")
+        assertThat(cuboidOn.intersect(cuboidOff)).isEqualTo(Some(parseCuboid("off x=4..5,y=4..5,z=4..5")))
+    }
+
+    @Test
+    fun `can compute differential intersection of 2 cuboids - off and on - 1`() {
+        val cuboidOn = parseCuboid("off x=1..15,y=1..15,z=1..15")
+        val cuboidOff = parseCuboid("on x=4..5,y=4..5,z=4..5")
+        assertThat(cuboidOn.intersect(cuboidOff)).isEqualTo(Some(parseCuboid("on x=4..5,y=4..5,z=4..5")))
+    }
+
+    @Test
+    fun `can compute volume of a cuboid`() {
+        assertThat(parseCuboid("on x=10..12,y=10..12,z=10..12").volume).isEqualTo(27)
+        assertThat(parseCuboid("off x=9..11,y=9..11,z=9..11").volume).isEqualTo(-27)
+    }
+
+    @Test
+    fun `can process create of differential cuboids`() {
+        val cuboids = listOf(parseCuboid("on x=10..12,y=10..12,z=10..12"), parseCuboid("on x=11..13,y=11..13,z=11..13"))
+        val processedCuboids = cuboids.process()
+        assertThat(processedCuboids).hasSize(3)
+        assertThat(processedCuboids).isEqualTo(
+            listOf(
+                Cuboid(x = 10..12, y = 10..12, z = 10..12, toggle = true),
+                Cuboid(x = 11..12, y = 11..12, z = 11..12, toggle = false),
+                Cuboid(x = 11..13, y = 11..13, z = 11..13, toggle = true)
+            )
+        )
+        assertThat(processedCuboids.volume).isEqualTo(46)
+    }
+
+    @Test
+    fun `can process create of differential cuboids - 2`() {
+        val cuboids = listOf(
+            parseCuboid("on x=10..12,y=10..12,z=10..12"),
+            parseCuboid("on x=11..13,y=11..13,z=11..13"),
+            parseCuboid("off x=9..11,y=9..11,z=9..11")
+        )
+        val processedCuboids = cuboids.process()
+        assertThat(processedCuboids).hasSize(6)
+        assertThat(processedCuboids).isEqualTo(
+            listOf(
+                Cuboid(x = 10..12, y = 10..12, z = 10..12, toggle = true),
+                Cuboid(x = 11..12, y = 11..12, z = 11..12, toggle = false),
+                Cuboid(x = 11..13, y = 11..13, z = 11..13, toggle = true),
+                Cuboid(x = 10..11, y = 10..11, z = 10..11, toggle = false),
+                Cuboid(x = 11..11, y = 11..11, z = 11..11, toggle = true),
+                Cuboid(x = 11..11, y = 11..11, z = 11..11, toggle = false)
+            )
+        )
+        assertThat(processedCuboids.volume).isEqualTo(38)
+    }
+
+    @Test
+    fun `can process create of differential cuboids - 3`() {
+        val cuboids = listOf(
+            parseCuboid("on x=10..12,y=10..12,z=10..12"),
+            parseCuboid("on x=11..13,y=11..13,z=11..13"),
+            parseCuboid("off x=9..11,y=9..11,z=9..11"),
+            parseCuboid("on x=10..10,y=10..10,z=10..10")
+        )
+        assertThat(cuboids.process().volume).isEqualTo(39)
+    }
+
+    @Test
+    fun `can solve example by cuboids differences`() {
+        assertThat(bigExample.process().volume).isEqualTo(2758514936282235)
+    }
+
+    @Test
+    fun `can solve puzzle by cuboids differences`() {
+        assertThat(puzzleInput.process().volume).isEqualTo(1134088247046731)
     }
 
     private fun processCuboids(cuboids: List<Cuboid>, from: Int, to: Int, size: Int): Long {
@@ -591,4 +702,67 @@ private val puzzleInput = """
     off x=20573..26063,y=-30310..-12158,z=-89541..-58150
     off x=64081..88596,y=-25922..-5947,z=16732..46694
     off x=79458..91210,y=-6760..4282,z=1518..13350
+    """.trimIndent().lines().map { parseCuboid(it) }
+
+private val bigExample = """
+    on x=-5..47,y=-31..22,z=-19..33
+    on x=-44..5,y=-27..21,z=-14..35
+    on x=-49..-1,y=-11..42,z=-10..38
+    on x=-20..34,y=-40..6,z=-44..1
+    off x=26..39,y=40..50,z=-2..11
+    on x=-41..5,y=-41..6,z=-36..8
+    off x=-43..-33,y=-45..-28,z=7..25
+    on x=-33..15,y=-32..19,z=-34..11
+    off x=35..47,y=-46..-34,z=-11..5
+    on x=-14..36,y=-6..44,z=-16..29
+    on x=-57795..-6158,y=29564..72030,z=20435..90618
+    on x=36731..105352,y=-21140..28532,z=16094..90401
+    on x=30999..107136,y=-53464..15513,z=8553..71215
+    on x=13528..83982,y=-99403..-27377,z=-24141..23996
+    on x=-72682..-12347,y=18159..111354,z=7391..80950
+    on x=-1060..80757,y=-65301..-20884,z=-103788..-16709
+    on x=-83015..-9461,y=-72160..-8347,z=-81239..-26856
+    on x=-52752..22273,y=-49450..9096,z=54442..119054
+    on x=-29982..40483,y=-108474..-28371,z=-24328..38471
+    on x=-4958..62750,y=40422..118853,z=-7672..65583
+    on x=55694..108686,y=-43367..46958,z=-26781..48729
+    on x=-98497..-18186,y=-63569..3412,z=1232..88485
+    on x=-726..56291,y=-62629..13224,z=18033..85226
+    on x=-110886..-34664,y=-81338..-8658,z=8914..63723
+    on x=-55829..24974,y=-16897..54165,z=-121762..-28058
+    on x=-65152..-11147,y=22489..91432,z=-58782..1780
+    on x=-120100..-32970,y=-46592..27473,z=-11695..61039
+    on x=-18631..37533,y=-124565..-50804,z=-35667..28308
+    on x=-57817..18248,y=49321..117703,z=5745..55881
+    on x=14781..98692,y=-1341..70827,z=15753..70151
+    on x=-34419..55919,y=-19626..40991,z=39015..114138
+    on x=-60785..11593,y=-56135..2999,z=-95368..-26915
+    on x=-32178..58085,y=17647..101866,z=-91405..-8878
+    on x=-53655..12091,y=50097..105568,z=-75335..-4862
+    on x=-111166..-40997,y=-71714..2688,z=5609..50954
+    on x=-16602..70118,y=-98693..-44401,z=5197..76897
+    on x=16383..101554,y=4615..83635,z=-44907..18747
+    off x=-95822..-15171,y=-19987..48940,z=10804..104439
+    on x=-89813..-14614,y=16069..88491,z=-3297..45228
+    on x=41075..99376,y=-20427..49978,z=-52012..13762
+    on x=-21330..50085,y=-17944..62733,z=-112280..-30197
+    on x=-16478..35915,y=36008..118594,z=-7885..47086
+    off x=-98156..-27851,y=-49952..43171,z=-99005..-8456
+    off x=2032..69770,y=-71013..4824,z=7471..94418
+    on x=43670..120875,y=-42068..12382,z=-24787..38892
+    off x=37514..111226,y=-45862..25743,z=-16714..54663
+    off x=25699..97951,y=-30668..59918,z=-15349..69697
+    off x=-44271..17935,y=-9516..60759,z=49131..112598
+    on x=-61695..-5813,y=40978..94975,z=8655..80240
+    off x=-101086..-9439,y=-7088..67543,z=33935..83858
+    off x=18020..114017,y=-48931..32606,z=21474..89843
+    off x=-77139..10506,y=-89994..-18797,z=-80..59318
+    off x=8476..79288,y=-75520..11602,z=-96624..-24783
+    on x=-47488..-1262,y=24338..100707,z=16292..72967
+    off x=-84341..13987,y=2429..92914,z=-90671..-1318
+    off x=-37810..49457,y=-71013..-7894,z=-105357..-13188
+    off x=-27365..46395,y=31009..98017,z=15428..76570
+    off x=-70369..-16548,y=22648..78696,z=-1892..86821
+    on x=-53470..21291,y=-120233..-33476,z=-44150..38147
+    off x=-93533..-4276,y=-16170..68771,z=-104985..-24507
     """.trimIndent().lines().map { parseCuboid(it) }
